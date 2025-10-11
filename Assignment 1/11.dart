d26df758a57241f047a93afaeb0ed95bbe14372f
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-11 20:00:10 +0600

    Create 11.dart

---
+++ Assignment 1/11.dart
@@ -0,0 +1,16 @@
+//Suppose, you often go to restaurant with friends and you have to split amount of bill. 
+//Write a program to calculate split amount of bill. 
+//Formula= (total bill amount) / number of people
+
+import 'dart:io';
+
+void main(){
+  stdout.write("Enter the total bill amount = ");
+  double t= double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the number of people = ");
+  int p= int.parse(stdin.readLineSync()!);
+
+  print("Formula= \$${(t) / p}");
+
+}
