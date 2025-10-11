c961c938c2d8872d3a0a9f9271f2836d1537aaf3
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-11 18:43:22 +0600

    Update 05.dart

--- Assignment 1/05.dart
+++ Assignment 1/05.dart
@@ -1,1 +1,10 @@
+// Write a program to print a square of a number using user input.
+
+import 'dart:io';
+
+void main(){
+  stdout.write("Enter a number : ");
+  double p = double.parse(stdin.readLineSync()!);
+  print("Square of a number= ${p*p}");
+}
 
