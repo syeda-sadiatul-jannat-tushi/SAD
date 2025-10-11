e68be7d342f4c8f5292f06796cec16d0dd31ee05
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-11 11:45:32 +0600

    Delete Assignment 01/practice 01 directory

--- Assignment 01/practice 01/02.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  //single quotes
-  print('Hello I am "John Doe"');
-  print( 'Hello I\'am "John Doe"');
-
-  //Double qutes
-  print("Hello I am \"John Doe\"");
-  print( "Hello I'am \"John Doe\"");
-}
