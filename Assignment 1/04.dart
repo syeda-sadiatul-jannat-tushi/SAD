2507524c890e74eb99adb48e1ace7aa6d98fbd90
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-11 18:05:52 +0600

    Update 04.dart

--- Assignment 1/04.dart
+++ Assignment 1/04.dart
@@ -1,1 +1,9 @@
+//Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
 
+void main(){
+  int p= 10;
+  int t= 5;
+  int r= 6;
+
+  print("Formula= ${(p * t * r) / 100}");
+}
