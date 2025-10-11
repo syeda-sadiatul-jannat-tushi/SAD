ad07c36f06631b99a1b2ef926b7ab7308fc730dd
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-11 20:29:15 +0600

    Update 11.dart

--- Assignment 1/11.dart
+++ Assignment 1/11.dart
@@ -11,6 +11,6 @@ void main(){
   stdout.write("Enter the number of people = ");
   int p= int.parse(stdin.readLineSync()!);
 
-  print("Formula= \$${(t) / p}");
+  print("Formula= ${(t) / p}");
 
 }
