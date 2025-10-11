6a6399436030a6b6a8f641961e9e2777143fa190
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-11 10:34:49 +0600

    Update 02.dart

--- Assignment 01/practice 01/02.dart
+++ Assignment 01/practice 01/02.dart
@@ -1,1 +1,9 @@
+void main(){
+  //single quotes
+  print('Hello I am "John Doe"');
+  print( 'Hello I\'am "John Doe"');
 
+  //Double qutes
+  print("Hello I am \"John Doe\"");
+  print( "Hello I'am \"John Doe\"");
+}
