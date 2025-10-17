afefe40287e9e08db1ad14ad9497903ddd370519
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-17 21:00:50 +0600

    Update 02.dart

--- Assignment 02/practice 02/02.dart
+++ Assignment 02/practice 02/02.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+void main(){
+    stdout.write("Enter the Number: ");
+    String c = (stdin.readLineSync()!);
+
+    c = c.toLowerCase();
+
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+        print("The character is a vowel");
+    }
+    else{
+        print("The character is a consonant");
+    }
+}
