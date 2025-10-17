0d417e1b9238dfff903dfb9a4a84290146183d58
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-10-17 20:03:41 +0600

    Update 01.dart

--- Assignment 02/practice 02/01.dart
+++ Assignment 02/practice 02/01.dart
@@ -1,1 +1,13 @@
+import 'dart:io';
 
+void main(){
+    stdout.write("Enter the Number: ");
+    int a = int.parse(stdin.readLineSync()!);
+
+    if( a%2 ==0){
+        print("NUmber is Even");
+    }
+    else{
+        print("NUmber is Ood");
+    }
+}
