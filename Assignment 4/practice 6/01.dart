e89a0a16cf2b034b6bd33dbc0d011ec64c4bd7e2
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-11-03 20:12:16 +0600

    Create 01.dart

---
+++ Assignment 4/practice 6/01.dart
@@ -0,0 +1,25 @@
+//Write a dart program to create a class Laptop with properties [id, name, ram] and create 3 objects of it and print all details.
+
+class Laptop{
+  int id;
+  String name;
+  int ram;
+
+  Laptop(this.id, this.name, this.ram);
+
+  void display(){
+    print("Laptop ID: $id");
+    print("Laptop Name: $name");
+    print("RAM: ${ram}GB");
+  }
+}
+
+void main(){
+  Laptop x= Laptop(1, 'Dell',8);
+  Laptop y= Laptop(2, 'HP' , 16);
+  Laptop z= Laptop(3, 'MAC' , 12);
+
+  x.display();
+  y.display();
+  z.display();
+}
