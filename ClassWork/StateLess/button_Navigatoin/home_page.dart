f049ec7ea18a99cdd536aa8f7da8a1241609eea9
Syeda Sadiatul Jannat Tushi <157502160+syeda-sadiatul-jannat-tushi@users.noreply.github.com>
2025-12-10 09:54:54 +0600

    Create home_page.dart

---
+++ ClassWork/StateLess/button_Navigatoin/home_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:profile_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HomePage"),
+        backgroundColor: Colors.blueGrey,
+        // leading: Icon(Icons.home),
+        // actions: [
+        //   IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        //   IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+        // ],
+      ),
+      endDrawer: NavigationDrawer(
+        children: [
+          DrawerHeader(
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blueGrey),
+              accountName: Text("Name"),
+              accountEmail: Text("Email"),
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.home),
+            title: Text("Homepage"),
+          ),
+          Divider(),
+          ListTile(
+            onTap: () {},
+            leading: Icon(Icons.person),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+        shape: CircleBorder(),
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/images/flutter.png", height: 200, width: 200),
+          Text(
+            "Homepage",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.blueGrey,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ProfilePage();
+                        },
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white,
+                    fixedSize: Size(150, 40),
+                  ),
+                  child: Text("Elevated"),
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Hello")),
+              SizedBox(width: 20),
+              OutlinedButton(onPressed: () {}, child: Text("Outlined")),
+              TextButton(onPressed: () {}, child: Text("Hello")),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
